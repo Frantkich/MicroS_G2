1d63dd2a333f6215c34290643fddae5a2d39c0dd
Ynovbanj0 <paul.bagnis@ynov.com>
2020-12-14 17:34:31 +0100

    premier test boot admin

--- microsg2/microsg2-bootAdmin/src/main/java/com/microsg2/microsg2bootAdmin/Microsg2BootAdminApplication.java
+++ microsg2/microsg2-bootAdmin/src/main/java/com/microsg2/microsg2bootAdmin/Microsg2BootAdminApplication.java
@@ -2,8 +2,13 @@ package com.microsg2.microsg2bootAdmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
 public class Microsg2BootAdminApplication {
 
 	public static void main(String[] args) {
