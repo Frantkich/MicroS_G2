6ca3d5408c7b36a24daf27975a942433df9fe53e
owain <ow.charlon@gmail.com>
2020-12-14 15:45:36 +0100

    Comment service debut

---
+++ microsg2/microsg2-comment/src/main/java/com/microsg2/microsg2comment/models/Comment.java
@@ -0,0 +1,73 @@
+package com.microsg2.microsg2comment.models;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "comment")
+public class Comment {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name="date")
+	private Date date;
+	
+	@Column(name="content")
+	private String content;
+	
+	@Column(name="author_id")
+	private int author_id;
+	
+	@Column(name="article_id")
+	private int article_id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getAuthor_id() {
+		return author_id;
+	}
+
+	public void setAuthor_id(int author_id) {
+		this.author_id = author_id;
+	}
+
+	public int getArticle_id() {
+		return article_id;
+	}
+
+	public void setArticle_id(int article_id) {
+		this.article_id = article_id;
+	}
+	
+	
+}
