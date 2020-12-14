b8331fc692e042ba8fecfbaf0c902674bae9d0cb
Ynovbanj0 <paul.bagnis@ynov.com>
2020-12-14 16:56:18 +0100

    Author MS

--- microsg2/microsg2-author/src/main/java/com/microsg2/microsg2author/Microsg2AuthorApplication.java
+++ microsg2/microsg2-author/src/main/java/com/microsg2/microsg2author/Microsg2AuthorApplication.java
@@ -2,20 +2,9 @@ package com.microsg2.microsg2author;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class Microsg2AuthorApplication {
-
-	@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Microsg2AuthorApplication.class, args);
